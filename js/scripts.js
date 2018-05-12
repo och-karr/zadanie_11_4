952db2ec320d32d3024653a90787b9ab84d69ef7
karolinneczka <okaa93@gmail.com>
2018-05-12 23:29:35 +0200

    Call the printInfo method in js file

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ function Phone(brand, price, color) {  //funkcja konstruujaca
 	this.price = price;
 	this.color = color;
 }
-Phone.prototype.printInfo = function() { // funkcja prototypowa
+Phone.prototype.printInfo = function() { // funkcja prototypowa - metoda printInfo
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
 
@@ -11,3 +11,7 @@ Phone.prototype.printInfo = function() { // funkcja prototypowa
 var iPhone6S = new Phone("Apple", 2250, "silver"); //obiekty na podstawie funkcji konstruujacej
 var samsungGalaxyS6 = new Phone("Samsung", 1494, "black");
 var onePlusOne = new Phone("OnePlus", 1728, "white");
+
+iPhone6S.printInfo(); //wywolanie metody printInfo dla obiektow
+samsungGalaxyS6.printInfo();
+onePlusOne.printInfo();
