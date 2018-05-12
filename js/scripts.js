d233087c075516a201b4902e7b671112c0be6ceb
karolinneczka <okaa93@gmail.com>
2018-05-12 23:22:43 +0200

    Add objects in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
-function Phone(brand, price, color) {
-	this.brand = brand;
+function Phone(brand, price, color) {  //funkcja konstruujaca
+	this.brand = brand;     //parametry
 	this.price = price;
 	this.color = color;
 }
-Phone.prototype.printInfo = function() {
+Phone.prototype.printInfo = function() { // funkcja prototypowa
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
+
+
+var iPhone6S = new Phone("Apple", 2250, "silver"); //obiekty na podstawie funkcji konstruujacej
+var samsungGalaxyS6 = new Phone("Samsung", 1494, "black");
+var onePlusOne = new Phone("OnePlus", 1728, "white");
